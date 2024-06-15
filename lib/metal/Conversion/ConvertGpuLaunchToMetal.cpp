3144bcb0dad7c9ea846d57fdff2a6f100a60062d
LoganHX <loganhx@outlook.it>
2024-06-15 12:27:37 +0200

    up

--- lib/metal/Conversion/ConvertGpuLaunchToMetal.cpp
+++ lib/metal/Conversion/ConvertGpuLaunchToMetal.cpp
@@ -1,13 +1,12 @@
-#include "metal/Conversion/MetalPasses.h"
 #include "metal/Conversion/GpuLaunchToMetal.h"
+#include "metal/Conversion/MetalPasses.h"
 
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
 #include "mlir/Dialect/GPU/IR/GPUDialect.h"
 #include "mlir/Dialect/GPU/TransformOps/GPUTransformOps.h"
-#include "mlir/Dialect/EmitC/IR/EmitC.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
-
 
 #include "mlir/Transforms/DialectConversion.h"
 #include <iostream>
@@ -17,8 +16,7 @@ namespace mlir::metal {
 #define GEN_PASS_DEF_CONVERTGPULAUNCHTOMETAL
 #include "metal/Conversion/MetalPasses.h.inc"
 
-bool isInsideGpuFunc(Operation *op) {
-  // Verifica se l'operazione è all'interno di un gpu.funcOp
+bool isInsideGpuSpace(Operation *op) {
   auto parent = op->getParentOp();
   while (parent) {
     if (isa<gpu::GPUFuncOp>(parent) || isa<gpu::GPUModuleOp>(parent)) {
@@ -27,7 +25,6 @@ bool isInsideGpuFunc(Operation *op) {
     parent = parent->getParentOp();
   }
   return false;
-
 };
 namespace {
 struct ConvertGpuLaunchToMetal
@@ -42,27 +39,24 @@ struct ConvertGpuLaunchToMetal
     target.addLegalDialect<emitc::EmitCDialect>();
     target.addLegalDialect<MetalDialect>();
     target.addLegalDialect<gpu::GPUDialect>();
-    
+
     target.addIllegalDialect<memref::MemRefDialect>();
     target.addIllegalOp<gpu::LaunchFuncOp>();
 
-    target.addDynamicallyLegalOp<memref::LoadOp>([](memref::LoadOp op) {
-        return isInsideGpuFunc(op);
-    });
-    target.addDynamicallyLegalOp<memref::StoreOp>([](memref::StoreOp op) {
-        return isInsideGpuFunc(op);
-    });
+    target.addDynamicallyLegalOp<memref::LoadOp>(
+        [](memref::LoadOp op) { return isInsideGpuSpace(op); });
+    target.addDynamicallyLegalOp<memref::StoreOp>(
+        [](memref::StoreOp op) { return isInsideGpuSpace(op); });
 
     RewritePatternSet patterns(&getContext());
-    mlir::metal::populateGpuLaunchToMetalConversionPatterns(patterns, &getContext());
+    mlir::metal::populateGpuLaunchToMetalConversionPatterns(patterns,
+                                                            &getContext());
 
     FrozenRewritePatternSet patternSet(std::move(patterns));
     if (failed(applyPartialConversion(getOperation(), target, patternSet)))
-      signalPassFailure(); 
-      
+      signalPassFailure();
   }
-  
 };
 
 } // end namespace
-} // nam
+} // namespace mlir::metal
